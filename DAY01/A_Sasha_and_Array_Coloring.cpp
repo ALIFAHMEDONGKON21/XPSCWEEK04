c5947c2a816bf6cd01014f2e7fba029227f8bf8f
MD ALIF <96830817+ALIFAHMEDONGKON21@users.noreply.github.com>
2023-10-22 18:46:05 +0600

    Add files via upload

---
+++ DAY01/A_Sasha_and_Array_Coloring.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll                              long long int
+#define     ull                             unsigned long long int
+#define     ui                              unsigned int
+//STL :
+#define     vi                              vector<int>
+#define     vb                              vector<bool>
+#define     vs                              vector<string>
+#define     vl                              vector<ll>
+#define     si                              set<int>
+#define     sl                              set<ll>
+#define     ip                              pair<int,int>
+#define     lp                              pair<ll,ll>
+#define     ips                             pair<string,int>
+#define     lps                             pair<string,ll>
+#define     ipc                             pair<char,int>
+#define     lpc                             pair<char,ll>
+#define     vip                             vector<ip>
+#define     PQ                              priority_queue
+#define     vlp                             vector<lp>
+#define     hashmap                         unordered_map
+#define     all(x)                          x.begin(),x.end()
+#define     forI                             (i,s,n) for(int i=s; i<n; i++)
+#define     in_range_back(i,x,y)            for(int i=y;i>=x;i--)
+#define     cin_the_array(x,o,n)            for(int i=o;i<=n;i++){cin>>x[i];}
+#define     cout_the_array(x,o,n)           for(int i=o;i<=n;i++){cout<<x[i]<<" ";}cout<<endl;
+
+
+
+//......Let's Start ........................//
+
+
+
+int main(){
+    
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+       int n;
+       cin>>n;
+
+       vector<ll> v(n);
+
+       for(int i=0; i<n; i++)
+       {
+        cin>>v[i];
+       }
+       sort(v.begin(),v.end());
+       int ans=0;
+       
+       ll l=0;
+       ll r=n-1;
+       while (r>l)
+       {
+        /* code */
+        ans +=(v[r]-v[l]);
+        l++;
+        r--;
+
+       }
+       cout<<ans<<endl;
+       
+
+    }
+    
+    
+    return 0;
+}
